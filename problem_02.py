281c5abbcb9688322381c32d0cfd0d37d240f536
Janna Roldan <162396417+Jhanna-R@users.noreply.github.com>
2025-02-10 14:55:37 +0800

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -5,11 +5,11 @@
 
 def main():
     
-    print("Square the Number Program")
-    
-    number = int(input("Enter a number: "))
-    
-    print(f"The square of {number} is {(number*number):.2f}")
+  print("Square the Number Program")
+
+  number = int(input("Enter a number: "))
+
+  print(f"The square of {number} is {(number * number):.2f}")
     
 if _name_ == "_main_":
     main()
