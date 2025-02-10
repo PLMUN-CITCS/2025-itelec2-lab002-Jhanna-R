c584313b2ef30308789c5e6d086ab15f71b0992c
Janna Roldan <162396417+Jhanna-R@users.noreply.github.com>
2025-02-10 20:54:11 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -3,7 +3,7 @@
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 def main():
-    print("Simple Calculator Program")
+   print("Simple Calculator Program")
 
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
