4939413ef5df84e4cc41598ef335a457df136a0a
Janna Roldan <162396417+Jhanna-R@users.noreply.github.com>
2025-02-10 20:51:08 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,13 +1,11 @@
-# Janna Roldan
+# JANNA ROLDAN
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
-
 def main():
-   print("Simple Calculator Program")
+    print("Simple Calculator Program")
 
     num1 = int(input("Enter 1st number: "))
-   
     num2 = int(input("Enter 2nd number: "))
 
     print(f"The sum is {num1 + num2}")
